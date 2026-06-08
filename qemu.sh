75e08debf363948bd725e7d7a1fcda2a70344383
Mr. Slippery <badc0ded@gmail.com>
2026-06-08 23:16:25 +0300

    Parametrize bzimage and rootfs

--- qemu.sh
+++ qemu.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ ! -f linux/arch/x86_64/boot/bzImage ]; then
+BZIMAGE=linux/arch/x86_64/boot/bzImage
+ROOTFS=buildroot/output/images/rootfs.ext2
+
+if [ ! -f "${BZIMAGE}" ]; then
   ./build_linux.sh
 fi
 
-if [ ! -f buildroot/output/images/rootfs.ext2 ]; then
+if [ ! -f "${ROOTFS}" ]; then
   ./build_rootfs.sh
 fi
 
 qemu-system-x86_64 -nographic \
-  -kernel linux/arch/x86_64/boot/bzImage \
-  -drive format=raw,file=buildroot/output/images/rootfs.ext2,if=virtio \
+  -kernel "${BZIMAGE}" \
+  -drive format=raw,file="${ROOTFS}",if=virtio \
   -append "root=/dev/vda console=ttyS0 nokaslr" \
   -m 4G \
   -smp 2 \
